3a2b7ee75f67f934ff048841c17a9737427c1cac
oluwajueda <oluwajuedaloade@gmail.com>
2023-04-25 07:30:20 +0100

    fetch data

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -8,34 +8,24 @@ import 'package:riverpd/user.dart';
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
-  void onSubmit(WidgetRef ref, String value) {
-    // ref.read(userProvider.notifier).updateName(value);
-    ref.read(userChangeNotifier).updateName(value);
-  }
-
-  void onSubmitAge(WidgetRef ref, String value) {
-    // ref.read(userProvider.notifier).updateAge(int.parse(value));
-    ref.read(userChangeNotifier).updateAge(int.parse(value));
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(userChangeNotifier).user;
+    final user = ref.watch(fetchUserProvider);
+
+    user.when(
+      data: (data) {},
+      error: (error, stackTrace) {},
+      loading: () {},
+    );
     // final user = ref.watch(userProvider.select((value) => value.name));
 
     return Scaffold(
       appBar: AppBar(),
       body: Column(
         children: [
-          TextField(
-            onSubmitted: (value) => onSubmit(ref, value),
-          ),
-          TextField(
-            onSubmitted: (value) => onSubmitAge(ref, value),
-          ),
           Center(
             child: Column(
-              children: [Text(user.name), Text(user.age.toString())],
+              children: [Text(""), Text("")],
             ),
           )
         ],
